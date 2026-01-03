4c9bf33ea0cb3305902d1dd921508bbec030de81
oy-yuji <oy.yuji96@gmail.com>
2026-01-03 08:07:59 -0500

    Add weight table display and improve data handling
    Introduces a table in the HTML to display weight entries and updates weights.js to fetch and render weight data from Supabase. Also improves the addWeight function and clears the input after submission.

--- weights.js
+++ weights.js
@@ -6,13 +6,42 @@ async function addWeight() {
   const date = new Date().toISOString();
   const { data, error } = await supabasePublicClient
     .from("weights")
-    .insert([{ date: date, weight: weight, run: false, strength: false }])
+    .insert([{ date: date, weight: weight, run: FALSE, strength: FALSE }])
     .select();
 
   if (error) {
     console.error(error);
-    document.getElementById("loadedPassword").innerText =
-      "Error connecting to Supabase";
-    return;
+  }
+
+  document.getElementById("weight").value = "";
+}
+
+//Display Table
+readWeights();
+async function readWeights() {
+  let { data: weights, error } = await supabasePublicClient
+    .from("weights")
+    .select("*");
+
+  const tableBody = document.querySelector("#weightTbl tbody");
+
+  weights.forEach(addRow);
+
+  function addRow(data) {
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+    <td>${data.date}</td>
+    <td>${data.weight}</td>
+    <td>
+    <input type="checkbox" id="runCheckBox" name="runCheckbox" />
+    ${data.run}
+    </td>
+    <td>
+      <input type="checkbox" id="strengthCheckBox" name="strengthCheckbox" />
+    ${data.strength}</td>
+  `;
+
+    tableBody.appendChild(row);
   }
 }
